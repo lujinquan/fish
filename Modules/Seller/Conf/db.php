a45eafa1f008470d470924375befa9f1802267ff
lucas51 <mylucas@aliyun.com>
2020-02-09 10:05:06 +0800

    附件备份

--- Modules/Seller/Conf/db.php
+++ Modules/Seller/Conf/db.php
@@ -3,7 +3,7 @@ return array(
 
 'DB_TYPE'   => 'mysql', // 数据库类型
 'DB_HOST'   => '595da0839009a.sh.cdb.myqcloud.com', // 服务器地址
-'DB_NAME'   => 'shop_hk_test', // 数据库名
+'DB_NAME'   => 'shop_hk', // 数据库名
 'DB_USER'   => 'root', // 用户名
 'DB_PWD'    => 'wfw@ctnm0404',  // 密码
 'DB_PORT'   => '17071', // 端口
